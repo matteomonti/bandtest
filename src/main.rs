00afed6d8501a2719811db60bb00454d80c6bd3f
Matteo Monti <matteo.monti@epfl.ch>
2022-07-10 09:53:35 +0000

    Add measurements

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use rand::prelude::*;
 
+use std::time::{Duration, Instant};
+
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt, Result},
     net::{TcpListener, TcpStream},
@@ -9,7 +11,6 @@ const BATCH_SIZE: usize = 1048576;
 
 #[tokio::main]
 async fn main() {
-    tokio::spawn(server());
     let _ = client().await;
 }
 
@@ -37,11 +38,33 @@ async fn serve(mut connection: TcpStream) -> Result<()> {
 }
 
 async fn client() -> Result<()> {
-    let mut connection = TcpStream::connect("127.0.0.1:1234").await.unwrap();
+    let mut connection = TcpStream::connect("172.31.38.206:1234").await.unwrap();
     let buffer = (0..BATCH_SIZE).map(|_| random()).collect::<Vec<u8>>();
 
-    for index in 0.. {
-        println!("Sending batch {}", index);
+    let mut last_print = Instant::now();
+    let mut last_value = 0;
+
+    let mut speeds = Vec::new();
+
+    for batch in 0.. {
+        if last_print.elapsed() >= Duration::from_secs(1) {
+            let speed = ((batch - last_value) as f64) / last_print.elapsed().as_secs_f64();
+            speeds.push(speed);
+
+            let average = statistical::mean(speeds.as_slice());
+
+            let std = if speeds.len() > 1 {
+                statistical::standard_deviation(speeds.as_slice(), None)
+            } else {
+                0.
+            };
+
+            println!("Speed: {} +- {} B/s", average, std);
+
+            last_print = Instant::now();
+            last_value = batch;
+        }
+
         connection.write_u32(BATCH_SIZE as u32).await?;
         connection.write_all(buffer.as_slice()).await?;
         let size = connection.read_u32().await?;
