0cb263318f9d42319e20a3d445078a705e737bc5
Matteo Monti <matteo.monti@epfl.ch>
2022-07-12 18:43:33 +0200

    Revert "Monitor established sessions"
    This reverts commit d53458693798ea5a7431dd26b02bf88a8b9a18d8.

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,7 @@ use talk::{
     net::{Session, SessionConnector, SessionListener},
 };
 
-use tokio::{
-    sync::{OwnedSemaphorePermit, Semaphore},
-    time,
-};
+use tokio::time;
 
 const RENDEZVOUS: &str = "172.31.5.210:9000";
 
@@ -26,8 +23,6 @@ const BATCH_SIZE: usize = 1100153;
 const BATCHES_PER_SESSION: usize = 1;
 
 const GRACE: usize = 5;
-
-const MAX_PERMITS: usize = usize::MAX >> 3;
 
 #[derive(Doom)]
 enum BandError {
@@ -104,11 +99,8 @@ async fn server(keychain: KeyChain) {
 
     let counter = Arc::new(RelaxedCounter::new(0));
 
-    let established = Arc::new(Semaphore::new(MAX_PERMITS));
-
     {
         let counter = counter.clone();
-        let established = established.clone();
 
         tokio::spawn(async move {
             let mut grace = GRACE;
@@ -134,13 +126,13 @@ async fn server(keychain: KeyChain) {
                         };
 
                         println!(
-                            "Received {} batches (instant: {} B / s) (average: {:.02} ± {:.02} B / s) (established sessions: {})",
-                            counter, speed, average, standard_deviation, (MAX_PERMITS - established.available_permits())
+                            "Received {} batches (instant: {} B / s) (average: {:.02} ± {:.02} B / s)",
+                            counter, speed, average, standard_deviation
                         );
                     } else {
                         println!(
-                            "Received {} batches (instant: {:.02} B / s) (established sessions: {})",
-                            counter, speed,(MAX_PERMITS - established.available_permits())
+                            "Received {} batches (instant: {:.02} B / s)",
+                            counter, speed,
                         );
                         grace -= 1;
                     }
@@ -155,25 +147,17 @@ async fn server(keychain: KeyChain) {
 
     loop {
         let (_, session) = listener.accept().await;
-
         let counter = counter.clone();
-        let established = established.clone();
-
-        let established_permit = established.acquire_owned().await.unwrap();
 
         tokio::spawn(async move {
-            if let Err(error) = serve(session, counter.as_ref(), established_permit).await {
+            if let Err(error) = serve(session, counter.as_ref()).await {
                 println!("{:?}", error);
             }
         });
     }
 }
 
-async fn serve(
-    mut session: Session,
-    counter: &RelaxedCounter,
-    _established_permit: OwnedSemaphorePermit,
-) -> Result<(), Top<BandError>> {
+async fn serve(mut session: Session, counter: &RelaxedCounter) -> Result<(), Top<BandError>> {
     for _ in 0..BATCHES_PER_SESSION {
         let buffer = session
             .receive_raw_bytes()
